d7390fb438b0471e723a6bf9936a0e92910182ae
Marcos <marcosbastidas301@gmail.com>
2021-08-27 15:31:51 -0400

    fecht_json-server

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Category } from '../Category';
 import { Lista, Item } from './styles';
-import information from '../../utils/information';
 
 export const ListOfCategory = () =>{
-  const {categories} =information;
+  const [categories, setCategories] = useState([
+    {
+      "id": 1,
+      "name": "cats",
+      "emoji": "🐱",
+      "cover": "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg",
+      "path": "/photos/cats"
+    },
+    {
+      "id": 2,
+      "name": "dogs",
+      "emoji": "🐶",
+      "cover": "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_dogs.jpg",
+      "path": "/photos/dogs"
+    },
+  ])
 
+  useEffect(function () {
+    window.fetch('http://localhost:3000/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <Lista>
       {
