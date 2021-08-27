76d111ff9be7255a6c18339f18eaee84c2c93747
Marcos <marcosbastidas301@gmail.com>
2021-08-27 00:00:47 -0400

    svg_component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory';
 import { GlobalStyle } from './GlobalStyles';
 import { ListOfPhotoCard } from './components/ListOfPhotoCard';
+import { Logo } from './components/Logo';
 
 const App = () => {
   return (
     <>
       <GlobalStyle/>
+      <Logo/>
       <ListOfCategory/>
       <ListOfPhotoCard/>
     </>
