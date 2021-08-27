b5bd0c7c20470fe27b6876bcf74f58dfc4d415b2
Marcos <marcosbastidas301@gmail.com>
2021-08-27 09:22:06 -0400

    animtion_photo-card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory';
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from './Styles/GlobalStyles';
 import { ListOfPhotoCard } from './components/ListOfPhotoCard';
 import { Logo } from './components/Logo';
 
