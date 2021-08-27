3d97850a18ef9c56e87abb1b3a1a088adeae50ac
Marcos <marcosbastidas301@gmail.com>
2021-08-26 20:10:58 -0400

    list_update-infomation

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Category } from '../Category';
 import { Lista, Item } from './styles';
+import information from '../../utils/information';
 
 export const ListOfCategory = () =>{
+  const {categories} =information;
 
   return (
     <Lista>
       {
-        [1,2,3,4,1,2,3,4].map((category, index) => (
-          <Item key={index} ><Category /> </Item>
+        categories.map((category) => (
+          <Item key={category.id} ><Category {...category} /> </Item>
         ))
       }
     </Lista>
