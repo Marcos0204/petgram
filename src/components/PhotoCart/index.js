45effdf4870c293d9e4a68fc3c7457558d44f86e
Marcos <marcosbastidas301@gmail.com>
2021-08-27 22:13:25 -0400

    photoCard_update

--- src/components/PhotoCart/index.js
+++ src/components/PhotoCart/index.js
@@ -1,23 +1,23 @@
-import React, {useEffect, useRef, useState, Fragment}  from 'react';
-import { ImgWrapper, Img, Button, Article } from './styles';
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Img, Button } from './styles'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-const DEFAULT_IMAGE= "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-
-export const PhotoCard = ({src=DEFAULT_IMAGE, id, likes=0})=>{
-  const [show, setShow] = useState(false);
-  const key = `like-${id}`
-  const [liked, setLiked]= useState(() =>{
-    try{
-      const like = window.localStorage.getItem(key);
-      return like
-    }catch (e){
-    }
-  } );
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null)
+  const [show, setShow] = useState(false)
+  const key = `like-${id}`
 
-  console.log(liked)
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (e) {
+      return false
+    }
+  })
+
   useEffect(function () {
     Promise.resolve(
       typeof window.IntersectionObserver !== 'undefined'
@@ -34,35 +34,33 @@ export const PhotoCard = ({src=DEFAULT_IMAGE, id, likes=0})=>{
       observer.observe(element.current)
     })
   }, [element])
-  const Icon = !liked ?  MdFavoriteBorder : MdFavorite;
 
-  const setLocalStorage= value =>{
-    try{
-      window.localStorage.setItem(key, value);
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
       setLiked(value)
     } catch (e) {
+      console.error(e)
     }
   }
 
   return (
     <Article ref={element}>
-      
-          {
-            
-            show && (
-            <Fragment>
-              <a href={`/detail/${id}`}>
-                <ImgWrapper>
-                  <Img src={src} />
-                </ImgWrapper>
-              </a>
-    
-              <Button onClick={()=> setLocalStorage(!liked) }>
-                <Icon size='32px' /> {likes} likes!
-              </Button>
-            </Fragment>)
-          }
-      
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size='32px' /> {likes} likes!
+          </Button>
+        </Fragment>
+      }
     </Article>
   )
 }
